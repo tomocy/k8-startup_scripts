512d16263ec0589303b2adac60b1ac1f6c0e1c01
tomocy <tomocy.dev@gmail.com>
2020-03-07 00:53:45 +0900

    update script for AKS
    This script gets credentials of the created cluster.

--- aks.sh
+++ aks.sh
@@ -7,8 +7,13 @@ az group create \
 
 echo 'create AKS'
 az aks create \
---name aks01 \
 --resource-group aks \
+--name aks01 \
 --node-count 1 \
 --enable-addons monitoring \
 --ssh-key-value ~/.ssh/aks/aks01/id_rsa.pub
+
+echo 'get AKS credentials'
+az aks get-credentials \
+--resource-group aks \
+--name aks01
